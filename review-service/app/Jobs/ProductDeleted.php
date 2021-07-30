d82aa590bd3a366761c0132f1bbcd9c65a75f246
Ji hoon Kang <wlgns5376@naver.com>
2021-07-30 18:25:56 +0900

    Review Service에 RabbitMQ 적용

---
+++ review-service/app/Jobs/ProductDeleted.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Review;
+
+class ProductDeleted implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if ($this->data) {
+            Review::where('product_id', $this->data['id'])->delete();
+            echo "Review has been deleted. product_id: ".$this->data['id'].PHP_EOL;
+        }
+    }
+}
